c7a8180f6e9062ac4a1eb302082738767cf969b3
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2023-09-05 14:23:50 +0300

    feat: add instrument product

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :instruments
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root 'store#home'
+  root 'instruments#index'
+  # root 'store#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
